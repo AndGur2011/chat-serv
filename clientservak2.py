483a1c19b700fd889f1851d28830124f31730c2f
Андрей <137599964+AndGur2011@users.noreply.github.com>
2025-10-26 11:20:59 +0200

    работаэт

--- clientservak2.py
+++ clientservak2.py
@@ -6,19 +6,22 @@ from PIL import Image
 window = CTk()
 window.title("Чатик 1.6")
 window.geometry("1080x700")
-                
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("localhost", 4144))
+
 def send():
     sms = enter.get()
     enter.delete(0, END)
-    print(sms)
+
     textbox.configure(state="normal")
     textbox.insert(END, sms + "\n")
     textbox.configure(state = "disabled")
 
+    client.send(sms.encode())
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 1028))
+
+
 
 # Потік для прийому повідомлень від сервера
 def listen():
@@ -26,7 +29,9 @@ def listen():
         try:
             msg = client.recv(1024).decode()
             if msg:
-                print("\nСервер:", msg)
+                textbox.configure(state="normal")
+                textbox.insert(END, msg + "\n")
+                textbox.configure(state = "disabled")
         except:
             pass
 
@@ -43,7 +48,7 @@ bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 chat_frame = CTkFrame(window, fg_color="#00716a", corner_radius=15, width=700, height=350,)  # рамка для чата
 chat_frame.place(x=190, y=40)
 chat_frame.pack_propagate(False)
-textbox = CTkTextbox(chat_frame, fg_color="#FFFFFF",state = "disabled", width = 650, height = 300)
+textbox = CTkTextbox(chat_frame, fg_color="#000000",state = "disabled", width = 650, height = 300)
 textbox.pack(pady = 20)
 
 enter = CTkEntry(window, width=850, height=50, placeholder_text="Введіть текст...", corner_radius=10, font=("Arial", 16))  # поле ввода
@@ -60,5 +65,5 @@ btn.place(x=960, y=420)
 window.mainloop()
 # Цикл для відправки повідомлень
 while True:
-    enter = CTkEntry(window, width=850, height=50, placeholder_text="Введіть текст...", corner_radius=10, font=("Arial", 16))
+    enter = CTkEntry(window, width=850, height=50, placeholder_text="Введіть текст...", corner_radius=10)
     client.send(enter.encode())
